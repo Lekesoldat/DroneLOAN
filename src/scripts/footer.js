d7c38d65933ade4756573ecfa2bd716cb83613e6
Magnus L. Holtet <magnulho@gmail.com>
2018-10-10 16:28:25 +0200

    Create footer script.

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -7,7 +7,20 @@
 
 import * as Rocket from './rocket.js';
 
-// Create the 
-export default Rocket.createElement(
+// To be displayed in the footer
+const fields = [
+  'Contact: contact@droneloan.com', 
+  'Address: Advokat Allé 5C', 
+  '&copy; DroneLOAN 2018'
+];
 
+// Create the footer.
+export default Rocket.createElement(
+  'ul',
+  null,
+  fields.map(field => Rocket.createElement(
+    'li', 
+    null, 
+    [field]
+  ))
 );
