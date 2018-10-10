621a1425433c86c3e53cc25db7f5f15f18ee7bf7
Magnus L. Holtet <magnulho@gmail.com>
2018-10-10 16:30:30 +0200

    Create script for nav bar and footer include.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,14 +2,22 @@
  FILE NAME: scripts/index.js
  WRITTEN BY: Magnus L. Holtet
  WHEN: October 2018
- PURPOSE: For modularity; Create navigation bar.
+ PURPOSE: For modularity; Create navigation bar and footer. 
  */
+
+// Import Rocket along with
 import * as Rocket from './rocket.js';
-import * as Turbo from './turbo.js';
-import routes from './routes.js';
+import navigation from './navigation.js';
+import footer from './footer.js';
 
 // Render navigation bar
 Rocket.render (
   document.getElementById('navigation'),
-  Turbo.renderRoutes(routes)
+  navigation
+);
+
+// Render footer
+Rocket.render(
+  document.getElementById('footer'),
+  footer
 );
--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,10 +2,11 @@
  FILE NAME: scripts/navigation.js
  WRITTEN BY: Magnus L. Holtet
  WHEN: October 2018
- PURPOSE: For modularity; Create navigation bar and footer. 
+ PURPOSE: For cleaner code. Not particulary necessary for the navigation bar, but for footer.
  */
 
-import * as Turbo from './turbo.js';
+import * as Orbit from './orbit.js';
 import routes from './routes.js';
 
-export default Turbo.renderRoutes(routes);
+// Export 'ul' with all list items embedded.
+export default Orbit.renderRoutes(routes);
