c40e6ba22a34f0e82edabbd13542c9aa4b16f63f
Magnus L. Holtet <magnulho@gmail.com>
2018-10-11 12:01:36 +0200

    Create file and template.

---
+++ src/scripts/drones.js
@@ -0,0 +1,31 @@
+/*
+ FILE NAME: scripts/drones.js
+ WRITTEN BY: Magnus and Mathias.
+ WHEN: October 2018
+ PURPOSE: Store information about each drone. Information will be used to render a drone specific page.
+ */
+
+// Array with 
+const drones = [
+  {
+    id: 0,
+    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'], 
+    name: '',
+    description: '',
+    specs: ['Spec one', 'Spec two', 'Spec three']
+  },
+  {
+    id: 1,
+    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
+    name: '',
+    description: '',
+    specs: ['Spec one', 'Spec two', 'Spec three']
+  },
+  {
+    id: '..',
+    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
+    name: '',
+    description: '',
+    specs: ['Spec one', 'Spec two', 'Spec three']
+  }
+];
