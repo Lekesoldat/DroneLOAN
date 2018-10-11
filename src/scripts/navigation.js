c3ba312c0363fef450ab847946bd65492ac1dd6e
Magnus L. Holtet <magnulho@gmail.com>
2018-10-11 12:01:48 +0200

    Restructure code. Fewer files.

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,11 +2,77 @@
  FILE NAME: scripts/navigation.js
  WRITTEN BY: Magnus L. Holtet
  WHEN: October 2018
- PURPOSE: For cleaner code. Not particulary necessary for the navigation bar, but for footer.
+ PURPOSE: Orbit: A self-made library extension of Rocket which handles navigation-bar rendering.
  */
 
-import * as Orbit from './orbit.js';
-import routes from './routes.js';
+import * as Rocket from './rocket.js';
 
-// Export 'ul' with all list items embedded.
-export default Orbit.renderRoutes(routes);
+const routes = [
+  {
+    path: './index.html',
+    title: Rocket.createElement(
+      'img',
+      {
+        src: './img/droneIcon.png',
+        alt: 'Drone Icon.',
+        width: 20,
+        heigth: 20
+      },
+      null
+    )
+  },
+  {
+    path: './drones.html',
+    title: 'OUR DRONES'
+  },
+  {
+    path: './videos.html',
+    title: 'VIDEO GALLERY'
+  },
+  {
+    path: './contact.html',
+    title: 'CONTACT'
+  },
+  {
+    path: './about.html',
+    title: 'ABOUT US'
+  }
+];
+
+
+// Creates a clickable element, specifies where it leads and its content (children)
+function renderLink(route) {
+  return Rocket.createElement(
+    'a',
+    {
+      href: route.path
+    },
+    [
+      route.title
+    ]
+  );
+}
+
+// Create a list item with an whatever as content.
+function renderListItem(route) {
+  return Rocket.createElement(
+    'li',
+    null,
+    [
+      renderLink(route)
+    ]
+  );
+}
+
+// Values is an array. Calls .render() on each item in values[]
+function renderList(routes) {
+  return Rocket.createElement(
+    'ul',
+    {
+      className: 'container'
+    },
+    routes.map(route => renderListItem(route))
+  );
+}
+
+export default renderList(routes);
