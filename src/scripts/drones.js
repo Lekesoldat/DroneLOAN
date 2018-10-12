349dddfe1f804d95fd369efc8ce78a46c88702f2
Magnus L. Holtet <magnulho@gmail.com>
2018-10-12 12:03:44 +0200

    Input dummy-content.

--- src/scripts/drones.js
+++ src/scripts/drones.js
@@ -10,22 +10,23 @@ const drones = [
   {
     id: 0,
     images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'], 
-    name: '',
-    description: '',
+    name: 'DJI Spark',
+    description: 'Very.',
     specs: ['Spec one', 'Spec two', 'Spec three']
   },
   {
     id: 1,
     images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
-    name: '',
-    description: '',
+    name: 'DJI Mavic Air',
+    description: 'Interesting.',
     specs: ['Spec one', 'Spec two', 'Spec three']
   },
   {
     id: '..',
     images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
-    name: '',
-    description: '',
+    name: 'Parrot Bepop 2',
+    description: 'Description.',
     specs: ['Spec one', 'Spec two', 'Spec three']
   }
 ];
+
