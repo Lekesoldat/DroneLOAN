fd16c510001a0cbebf4c195efb7a080529331a5b
Magnus L. Holtet <magnulho@gmail.com>
2018-10-22 14:04:55 +0200

    Change drone.image format.

--- src/scripts/drones.js
+++ src/scripts/drones.js
@@ -11,21 +11,60 @@ import * as Flare from './flare.js';
 const drones = [
   {
     id: 0,
-    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'], 
+    images: [
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'First image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Second image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Third image'
+      }
+    ],
     name: 'DJI Spark',
     description: 'Very.',
     specs: ['Spec one', 'Spec two', 'Spec three']
   },
   {
     id: 1,
-    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
+    images: [
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'First image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Second image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Third image'
+      }
+    ],
     name: 'DJI Mavic Air',
     description: 'Interesting.',
     specs: ['Spec one', 'Spec two', 'Spec three']
   },
   {
     id: '..',
-    images: ['./img/DRONENAVN.jpg', './img/DRONENAVN.jpg', './img/DRONENAVN.jpg'],
+    images: [
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'First image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Second image'
+      },
+      {
+        src: './img/DRONENAVN.jpg',
+        alt: 'Third image'
+      }
+    ],
     name: 'Parrot Bepop 2',
     description: 'Description.',
     specs: ['Spec one', 'Spec two', 'Spec three']
@@ -59,8 +98,8 @@ if (drone) {
           Spark.createElement(
             'img',
             {
-              src: image,
-              alt: drone.name
+              src: image.src,
+              alt: image.alt
             },
             null
           )
