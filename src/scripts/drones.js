85622cbf8856e04902ba47e4a5b947d576d7107e
Magnus L. Holtet <magnulho@gmail.com>
2018-10-22 19:21:41 +0200

    Create self-creating carousel. Add new render functions, update paths and change mapping.

--- src/scripts/drones.js
+++ src/scripts/drones.js
@@ -2,74 +2,12 @@
  FILE NAME: scripts/drones.js
  WRITTEN BY: Magnus.
  WHEN: October 2018
- PURPOSE: Store information about each drone. Information will be used to render a drone specific page.
+ PURPOSE: Render the drone specific page based on which drone was clicked on 'Our Drones'.
  */
 
 import * as Spark from './spark.js';
 import * as Flare from './flare.js';
-
-const drones = [
-  {
-    id: 0,
-    images: [
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'First image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Second image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Third image'
-      }
-    ],
-    name: 'DJI Spark',
-    description: 'Very.',
-    specs: ['Spec one', 'Spec two', 'Spec three']
-  },
-  {
-    id: 1,
-    images: [
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'First image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Second image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Third image'
-      }
-    ],
-    name: 'DJI Mavic Air',
-    description: 'Interesting.',
-    specs: ['Spec one', 'Spec two', 'Spec three']
-  },
-  {
-    id: '..',
-    images: [
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'First image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Second image'
-      },
-      {
-        src: './img/DRONENAVN.jpg',
-        alt: 'Third image'
-      }
-    ],
-    name: 'Parrot Bepop 2',
-    description: 'Description.',
-    specs: ['Spec one', 'Spec two', 'Spec three']
-  }
-];
+import drones from './droneInfo.js';
 
 // Grabs query parameters
 const params = new URLSearchParams(document.location.search);
@@ -90,21 +28,8 @@ if (drone) {
       title: drone.name,
       about: drone.description,
 
-      // <li><img></li>
-      images: drone.images.map(image => Spark.createElement(
-        'li',
-        null,
-        [
-          Spark.createElement(
-            'img',
-            {
-              src: image.src,
-              alt: image.alt
-            },
-            null
-          )
-        ]
-      )),
+      bigImage: renderCarouselElement(drone.images),
+      miniImage: renderMiniSection(drone.images),
 
       // <li></li>
       specs: drone.specs.map(spec => Spark.createElement(
@@ -117,3 +42,125 @@ if (drone) {
     }
   );
 }
+
+/* Image carousel scripts */
+
+// Used for creation of the big image in the carousel
+function renderImage(image) {
+  return Spark.createElement(
+    'img',
+    {
+      src: image.src,
+      alt: image.alt,
+    },
+    null
+  );
+}
+
+/* Returns the following HTML
+  <div class="mySlides carousel">
+    <img src="image.src" alt="image.alt">
+  </div>
+*/
+function renderBigImageDiv(image) {
+  return Spark.createElement(
+    'div',
+    {
+      className: 'mySlides carousel'
+    },
+    [
+      renderImage(image)
+    ]
+  );
+}
+
+/* Returns the following HTML
+  <div class="col-12">
+    <div class="mySlides carousel">
+      <img src="path">
+    </div>
+  </div>
+*/
+function renderCarouselElement(images) {
+  return Spark.createElement(
+    'div',
+    {
+      className: 'col-12'
+    },
+    images.map(image => renderBigImageDiv(image))
+  );
+}
+
+/* Returns the following HTML
+  <img class="blur cursor" src="image.path" alt="image.alt" onclick="currentSlide(index + 1)">
+*/
+function renderMiniImage(image, index) {
+  return Spark.createElement(
+    'img',
+    {
+      src: image.src,
+      alt: image.alt,
+      className: 'blur cursor',
+      onclick: () => currentSlide(index + 1)
+    },
+    null
+  );
+}
+
+/* Creates
+  <div class="row">
+
+    <div class="col-4 mini-image">
+      <img class="blur cursor" src="image.path" alt="image.alt" onclick="currentSlide(index + 1)">
+    </div>
+
+    .
+    .
+    .
+
+*/
+function renderMiniSection(images) {
+  return Spark.createElement(
+    'div',
+    {
+      className: 'row'
+    },
+    images.map((image, index) => Spark.createElement(
+      'div',
+      {
+        className: 'col-4 mini-image'
+      },
+      [
+        renderMiniImage(image, index)
+      ]
+    ))
+  );
+}
+
+/*  - - - - - - - - - - - CAROUSEL SCRIPT - - - - - - - - - - - */
+let imageIndex = 1;
+const currentSlide = n => showSlides(imageIndex = n);
+
+const showSlides = n => {
+  // Grab elements from page
+  const slides = document.getElementsByClassName('mySlides');
+  const miniImages = document.getElementsByClassName('blur');
+
+  // Hide all slides
+  for (let slide of slides) {
+    slide.style.display = 'none';
+  }
+
+  // Make all miniimages inactive
+  for (let image of miniImages) {
+    image.className = image.className.replace(' active', '');
+  }
+
+  // Display first image and set active
+  slides[imageIndex - 1].style.display = 'block';
+  miniImages[imageIndex - 1].className += ' active';
+};
+
+// Render first image on page load
+showSlides(imageIndex);
+
