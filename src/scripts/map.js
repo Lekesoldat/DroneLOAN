f9f992699372bfeb36b6aa55df46f377c45d4573
aa1202 <andreas.amundsen123@gmail.com>
2018-10-26 18:18:30 +0200

    Remove comment

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,36 +1,3 @@
-/* let lang;
-
-document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelectorAll('#googleMap').length > 0)
-  {
-    if (document.querySelector('html').lang) {
-      lang = document.querySelector('html').lang;
-    } else {
-      lang = 'en';
-    }
-
-    var js_file = document.createElement('script');
-    js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyDUD161uyW5j432MBN_AN-MTGHSz50xl30&language=' + lang;
-    document.getElementsByTagName('head')[0].appendChild(js_file);
-  }
-});
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('googleMap'),  {
-    // Initialize center and zoom
-    storeLatLng: {lat: 63.417831662, lng: 10.401831726},
-    zoom: 1
-  });
-}
-
- */
-
-
-
-
 function initMap(){
   var location = {lat: 63.417831662, lng: 10.401831726};
   var map = new google.maps.Map(document.getElementById('googleMap'), {
